2d3a7a888f4755bf540f0c0c614b042f86d65169
Ryan Kinzer <ryank@nezperce.org>
2018-06-12 14:15:20 -0600

    re-worked Joseph's original load file and .Rmd file, and then changed some file paths to store data files in the data folder

--- R/02_load_chinook_bull_data.R
+++ R/02_load_chinook_bull_data.R
@@ -3,109 +3,104 @@
 # Joseph Feldhaus (ODFW) & Ryan Kinzer (NPT)
 # Date: 6/11/18     Modified: 6/11/18
 #------------------------------------------------------------------------------
-# The purpose of this script is to combine Chinook and Bull Trout detections within the Imnaha River basin into a single file.
-# The output file is processed with functions found in PITcleanr.  Noteably, assignNodes(truncate =T) to shorten the file size.
-# The output file is: PITcleanr_2018_chs_bull.rda which is also saved as an xlsx file for easier viewing.
-# At the very end of the script, some objects are removed from the R environment (e.g., removed chs_url, bull_obs_import)
+# The purpose of this script is to combine Chinook and Bull Trout detections
+# within the Imnaha River basin into a single file. The output file is processed
+# with functions found in PITcleanr.  Noteably, assignNodes(truncate =T) to
+# shorten the file size. The output file is: PITcleanr_2018_chs_bull.rda which
+# is also saved as an xlsx file for easier viewing. At the very end of the
+# script, some objects are removed from the R environment (e.g., removed
+# chs_url, bull_obs_import)
 #------------------------------------------------------------------------------
 
+# load packages
 library(tidyverse)
 library(lubridate)
 library(xlsx)
-library(PITcleanr)#devtools::install_github("KevinSee/PITcleanr", build_vignettes = TRUE)
+library(PITcleanr) #devtools::install_github("KevinSee/PITcleanr", build_vignettes = TRUE)
 
-##works with R version 3.4.2 but chokes with version 3.5
-#chs_obs_import<-read.csv('ftp://ftp.ptagis.org/MicroStrategyExport/feldhauj/2018_Imnaha_CompleteTagHistory.csv', colClasses="character",fileEncoding = "UTF-16LE")
-#bull_obs_raw <- read.csv('ftp://ftp.ptagis.org/MicroStrategyExport/rkinzer/Imnaha_Bull_Complete_Tag_History.csv', colClasses="character",fileEncoding = "UTF-16LE")
-##https://stackoverflow.com/questions/50070113/r-3-5-read-csv-not-able-to-read-utf-16-csv-file?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+# load chinook and bull trout data from PTAGIS ftp servers - code works with R version 3.5
+chs_url <- 'ftp://ftp.ptagis.org/MicroStrategyExport/feldhauj/2018_Imnaha_CompleteTagHistory.csv'#Chinook file
+download.file(chs_url, paste0("./data/", basename(chs_url)))
+chs_obs_import <- read.csv(paste0("./data/", basename(chs_url)), skip = 0, colClasses="character",
+                           fileEncoding = "UTF-16", sep = ",", header = TRUE)
 
+bull_url <- 'ftp://ftp.ptagis.org/MicroStrategyExport/rkinzer/Imnaha_Bull_Complete_Tag_History.csv'#Bull trout file
+download.file(bull_url, paste0("./data/", basename(bull_url)))
+bull_obs_import <- read.csv(paste0("./data/", basename(bull_url)), skip = 0, colClasses="character",
+                            fileEncoding = "UTF-16", sep = ",", header = TRUE)
 
-#########Fixing read.csv code to work with R version 3.5
-chs_url<-'ftp://ftp.ptagis.org/MicroStrategyExport/feldhauj/2018_Imnaha_CompleteTagHistory.csv'#Chinook file
-download.file(chs_url, basename(chs_url))
-chs_obs_import<-read.csv(basename(chs_url), skip = 0, colClasses="character",fileEncoding = "UTF-16", sep = ",", header = TRUE)
-
-bull_url<-'ftp://ftp.ptagis.org/MicroStrategyExport/rkinzer/Imnaha_Bull_Complete_Tag_History.csv'#Bull trout file
-download.file(bull_url, basename(bull_url))
-bull_obs_import<-read.csv(basename(bull_url), skip = 0, colClasses="character",fileEncoding = "UTF-16", sep = ",", header = TRUE)
-
-
-###Select the 7 key fields + a couple extra co-variates (mark site, mark file, mark species)
-chs_obs_raw<-chs_obs_import%>%select(Tag.Code,
-                                     Mark.Rear.Type.Name,
-                                     Release.Site.Code=Release.Site.Code.Value,
+# Create full dataset
+chs_bull_obs_raw <- bind_rows(chs_obs_import %>%
+                                select(Tag.Code,
+                                     Mark.Species = Mark.Species.Name,
+                                     Mark.Rear.Type = Mark.Rear.Type.Name,
+                                     Mark.File = Mark.File.Name,
+                                     Release.Site.Code = Release.Site.Code.Value,
+                                     Release.Date = Release.Date.MMDDYYYY,
+                                     Event.Site.Code.Value,                                     
                                      Event.Date.Time.Value,
-                                     Event.Site.Code.Value,
                                      Antenna.ID,
-                                     Antenna.Group.Configuration.Value,
-                                     CTH.Count, 
-                                     Mark.File=Mark.File.Name,
-                                     Mark.Rear.Type=Mark.Rear.Type.Name,
-                                     Mark.Species=Mark.Species.Name)
+                                     Antenna.Group.Configuration.Value
+                                     ),
+                              bull_obs_import %>%
+                                select(Tag.Code = Tag,
+                                           Mark.Species,      
+                                           Mark.Rear.Type, 
+                                           Mark.File,
+                                           Release.Site.Code, 
+                                           Release.Date,
+                                           Event.Site.Code.Value=Event.Site.Code,                                          
+                                           Event.Date.Time.Value=Event.Date.Time, 
+                                           Antenna.ID=Antenna,
+                                           Antenna.Group.Configuration.Value=Antenna.Group.Configuration
+                                          )
+                              ) %>%
+                    mutate(Antenna.Group.Configuration.Value = as.numeric(Antenna.Group.Configuration.Value),
+                           Origin = ifelse(grepl("Hatchery", Mark.Rear.Type), "Hat",
+                                           ifelse(grepl("Wild", Mark.Rear.Type), "Nat", "Unk")),
+                           Release.Date = mdy(Release.Date))
 
-bull_obs_raw<-bull_obs_import%>%select(Tag.Code=Tag,
-                                     Mark.Rear.Type, 
-                                     Release.Site.Code, 
-                                     Event.Date.Time.Value=Event.Date.Time, 
-                                     Event.Site.Code.Value=Event.Site.Code,
-                                     Antenna.ID=Antenna,
-                                     CTH.Count,
-                                     Antenna.Group.Configuration.Value=Antenna.Group.Configuration,
-                                     Mark.File,
-                                     Mark.Rear.Type,
-                                     Mark.Species)
+# create dataset of lost covariates
+ExtraData <- chs_bull_obs_raw %>%
+  select(TagID = Tag.Code, Mark.Species, Origin, Release.Site.Code, Release.Date) %>%
+  distinct()
 
-#combine the Chinook and bull trout data into a single file
-chs_bull_obs_raw<-rbind(chs_obs_raw,bull_obs_raw)
-chs_bull_obs_raw$Antenna.Group.Configuration.Value<-as.numeric(chs_bull_obs_raw$Antenna.Group.Configuration.Value)#needs to be numeric
+# modify field names by replacing the "." with a space for PITcleanR
+names(chs_bull_obs_raw) <- gsub("\\.", " ",names(chs_bull_obs_raw))
 
-###The functions seem to ignore/silently drop extra fields.  Append/Merge these extra fields at the end.
-ExtraData<-chs_bull_obs_raw%>%select(TagID=Tag.Code, Mark.Species, Mark.Rear.Type,Release.Site.Code)%>%distinct()
-ExtraData$Origin<-"Unk"
-ExtraData$Origin[str_detect(ExtraData$Mark.Rear.Type,"Hatchery")]<-"Hat"
-ExtraData$Origin[str_detect(ExtraData$Mark.Rear.Type,"Wild")]<-"Nat"
+# subset distinct list of tag codes and name appropriately for PITcleanR
+chs_bull_tags <- chs_bull_obs_raw %>% 
+  distinct(`Tag Code`) %>%
+  mutate(TagID = `Tag Code`,TrapDate=as.Date("2018-01-01"))
 
-#####modify field names by replacing the "." with a space
-names(chs_bull_obs_raw) <- gsub("\\.", " ",names(chs_bull_obs_raw))#replace . with a space to be consistent with PITcleanr
-chs_bull_tags<-chs_bull_obs_raw%>%select(TagID=`Tag Code`)%>%distinct()%>%mutate(Tag=TagID,TrapDate=as.Date("2018-01-01"))
+# load PITcleanR configuration files run on June 5th
+load("./data/config_data_20180605.rda")
 
-####These 3 steps do not require the PIT Tag observation data, but are necessary to continue the script
-#load("C:/Rprojects/Imnaha_PITtracking/data/config_data_20180605.rda")
-my_config<-buildConfig()#Compile metadata from all MRR and interogation sites from PTAGIS
-site_df<-writeLGRNodeNetwork()#
-parent_child <- createParentChildDf(site_df, my_config, startDate = 20180101)
-
+# assign node names to each observation and truncate
 valid_obs <- assignNodes(valid_tag_df = chs_bull_tags,
                          observation = chs_bull_obs_raw,
                          configuration = my_config,
                          parent_child_df = parent_child,
                          truncate = T)
 
-valid_paths<-getValidPaths(parent_child_df=parent_child,root_site=NULL)
-node_order<-createNodeOrder(valid_paths = valid_paths, configuration =my_config, site_df = site_df, step_num = 3)
-
+# assign direction and valid observation calls
 proc_obs <- writeCapHistOutput(valid_obs,
                                valid_paths,
                                node_order,
                                save_file = FALSE)
 
-PITcleanr_2018_chs_bull<-full_join(proc_obs,ExtraData,by="TagID")%>%select(-TrapDate)%>%
-  select(TagID,Mark.Species,Origin,Mark.Rear.Type,Release.Site.Code,firstObsDateTime=ObsDate,lastObsDateTime=lastObsDate,everything())%>%droplevels()
+# join covariate data with processed capture histories
+PITcleanr_2018_chs_bull <- right_join(ExtraData, proc_obs, ExtraData, by="TagID") %>%
+  select(-TrapDate) %>%
+  select(TagID, Mark.Species, Origin, Release.Site.Code,
+         firstObsDateTime = ObsDate, lastObsDateTime = lastObsDate,
+         everything()) %>%
+  droplevels()
+
 
 write.xlsx2(as.data.frame(PITcleanr_2018_chs_bull),"./data/PITcleanr_2018_chs_bull.xlsx",row.names=FALSE) 
+
 saveRDS(PITcleanr_2018_chs_bull,"./data/PITcleanr_2018_chs_bull.rds")
 
-###Clean the R-environment###
-###When the script is sourced, the following objects show up and clutter the workflow. These are remove with rm()
-
-rm(bull_url,
-   chs_url,
-   bull_obs_import,
-   bull_obs_raw,
-   chs_bull_obs_raw,
-   chs_bull_tags,
-   chs_obs_import,
-   chs_obs_raw,
-   ExtraData,
-   valid_obs,
-   proc_obs)
+# Clean the R-environment
+rm(list = ls())
